14c4804cb58fa212b2d76352b2206e52a8951131
liucuicui <liucuicui@mskj.com>
2020-01-06 17:42:00 +0800

    first commit

---
+++ src/main/java/Service/HelloHandler.java
@@ -0,0 +1,52 @@
+package Service;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+
+public class HelloHandler extends TextWebSocketHandler {
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
+        //接收到客户端消息时调用
+        System.out.println("text message: " + session.getId() + "-" + message.getPayload());
+        String text = "get message "+session.getId();
+        System.out.println(text);
+        session.sendMessage(new TextMessage(text.getBytes()));
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session)
+            throws Exception {
+        // 与客户端完成连接后调用
+        System.out.println("afterConnectionEstablished");
+        System.out.println("getId:" + session.getId());
+        System.out.println("getLocalAddress:" + session.getLocalAddress().toString());
+        System.out.println("getTextMessageSizeLimit:" + session.getTextMessageSizeLimit());
+        System.out.println("getUri:" + session.getUri().toString());
+        System.out.println("getPrincipal:" + session.getPrincipal());
+        session.sendMessage(new TextMessage("hello".getBytes()));
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession session,
+                                     Throwable exception) throws Exception {
+// 消息传输出错时调用
+        System.out.println("handleTransportError");
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session,
+                                      CloseStatus closeStatus) throws Exception {
+// 一个客户端连接断开时关闭
+        System.out.println("afterConnectionClosed");
+    }
+
+    @Override
+    public boolean supportsPartialMessages() {
+// TODO Auto-generated method stub
+        return false;
+    }
+}
